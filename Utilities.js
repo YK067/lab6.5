cf34a3ad00480fdc03fda5a7baf149d56510067c
YK067 <157444759+YK067@users.noreply.github.com>
2024-04-25 03:32:15 +0000

    update index.html

--- Utilities.js
+++ Utilities.js
@@ -2,18 +2,21 @@
  
   window.onload = function(){
     ScrollReveal().reveal('.img1',{delay:1000});
-    ScrollReveal().reveal('.img2',{delay:1000});
-    ScrollReveal().reveal('.img3',{delay:1000});
-    ScrollReveal().reveal('.img4',{delay:1000});
-    ScrollReveal().reveal('.img5',{delay:1000});
-
-
-
-
-
 
   };
+  window.onload = function(){
+    ScrollReveal().reveal('.img2',{delay:1000});
+  };
+  window.onload = function(){
+      ScrollReveal().reveal('.img3',{delay:1000});
+  };
  
+  window.onload = function(){
+    ScrollReveal().reveal('.img4',{delay:1000});
+};
+window.onload = function(){
+  ScrollReveal().reveal('.img5',{delay:1000});
+};
 
 
   
